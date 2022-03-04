d297f4a518bc002dd531512f41686daf1650df7b
florent <florent.bellier@hotmail.fr>
2022-03-04 11:03:25 +0100

    Ajoutez des fichiers projet.

---
+++ Sonnette/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Sonnette.RaspPi;
+
+public class Program {
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
